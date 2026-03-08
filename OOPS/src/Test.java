e161a9eab4caff4012cf28263734e266b9f113d8
Pradeep_Mhaske <mhaskepradeep249@gmail.com>
2026-03-08 14:34:39 +0530

    Oops project

---
+++ OOPS/src/Test.java
@@ -0,0 +1,23 @@
+
+public class Test {
+	public static void main(String[] args) {
+		//overloading     Overriding
+		Parent p = new Child ();
+		p.m1(10);
+	}
+
+}
+class Parent {
+	public void m1(int a) {
+		System.out.println("Parent");
+	}
+}
+class Child extends Parent{
+	public void m1(String name) {
+		System.out.println("Child");
+	}
+	
+}
+
+
+
